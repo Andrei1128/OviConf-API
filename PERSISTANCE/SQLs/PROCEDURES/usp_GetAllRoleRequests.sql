6b1b888949511b086623ab0958f166e5b181cd0d
Andrei <nicolaev.andrei09@gmail.com>
2024-01-10 22:51:13 +0200

    modif GetAllRole

--- PERSISTANCE/SQLs/PROCEDURES/usp_GetAllRoleRequests.sql
+++ PERSISTANCE/SQLs/PROCEDURES/usp_GetAllRoleRequests.sql
@@ -3,8 +3,9 @@ GO
 CREATE OR ALTER PROCEDURE usp_GetAllRoleRequests
 AS
 BEGIN
-	SELECT r.Id ,u.Name as UserName, r.RequestedAt, r.Role as RequestedRole, r.ConferenceId 
-	FROM tbl_Roles as r INNER JOIN tbl_Users as u
-	ON r.UserId = u.Id
-	AND r.IsAccepted != 1;
+    SELECT r.Id ,u.Name as UserName, r.RequestedAt, r.Role as RequestedRole, r.ConferenceId 
+    FROM tbl_Roles as r INNER JOIN tbl_Users as u
+    ON r.UserId = u.Id
+    AND r.IsAccepted IS NULL OR r.IsAccepted = 0
+    ORDER BY r.RequestedAt;
 END;
